7f25e567adad063d253965e1936830ec70133bf6
이준형 <oneofakind1112@gmail.com>
2026-07-04 03:05:32 +0900

    Stabilize final archive layout layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './JerboaFinalRepair.css';
 
 console.log('index.tsx starting');
 const container = document.getElementById('root');
